7bdf328a4c418e30a112e250fbc88b24aba53916
somilshekhar <129623166+somilshekhar@users.noreply.github.com>
2025-05-14 13:26:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+function mapLetters(word) {
+  const result = {};
+
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i];
+    
+    if (result[letter]) {
+      result[letter].push(i);
+    } else {
+      result[letter] = [i];
+    }
+  }
+
+  return result;
+}
